e7078cfb761c000ce4dbf255dcdcb6aa2a8c116b
bairon-vasquez <baironvasquez1604@gmail.com>
2024-09-10 10:57:49 -0500

    implementacion de potencia

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def sumar(x, y):
     return x + y
 
 def restar(x, y):
-    return x - y
-
+    return x + y
+def division(x,y):
+     if y == 0:
+         return 0
+     else:
+         return x/y
+def potenciacion(x):
+     return x*x
 
 respuesta = sumar(5, 7)
 print(respuesta)
